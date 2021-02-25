137ebbb68f189fa5599f02d74bfad2cb42641513
Maitreya <mtsahu1995@gmail.com>
2021-02-25 23:41:10 +0530

    task 2 completed

---
+++ task-2/task-2.js
@@ -0,0 +1,22 @@
+// Try the rest countries api. 
+// Extract and print the total population of all the countries in the console. use the html template. 
+// https://restcountries.eu/rest/v2/all
+
+// 1. Create a new XMLHttpRequest object
+let xhr = new XMLHttpRequest();
+
+// 2. Configure it
+xhr.open('GET', 'https://restcountries.eu/rest/v2/all');
+
+// 3. Send the request over the network
+xhr.send();
+
+// 4. This will be called after the response is received
+xhr.onload = function () {
+    if (xhr.status != 200) { // analyze HTTP status of the response
+        console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+        let countryData = JSON.parse(xhr.response);
+        console.log(countryData.reduce((acc, cur) => acc + cur.population, 0)); // response is the server response
+    }
+};
